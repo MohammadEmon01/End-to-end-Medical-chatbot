5fc8bc8084012bfae34b24d0a68a106b64fa7475
MohammadEmon01 <emon01234567890@gmail.com>
2025-10-24 18:45:28 +0600

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Generative AI_Project",
+    version = "0.1",
+    author = "ALI AJGOR EMON",
+    author_email = "aliajgoremon@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+)
+
+    
